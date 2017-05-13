1cd3fe4b0424cf6ad3a8a9b2b98876ab4b4ef3c6
mcjohnpj143 <pauljohn.peligro@gmail.com>
2017-05-13 15:08:57 +0800

    Add files via upload

---
+++ JS/script1.js
@@ -0,0 +1,6 @@
+document.querySelector("#appdemo2").innerHTML=
+'<button type="button" class="btn btn-primary" id="clickMe">Click Me</button>';
+$('#clickMe').click(function(){
+	alert("I am Paul :)");
+
+});
